c61a7f697d66e6506a016f562f1758632b8a750e
Lee Presswood <pressedlee123@gmail.com>
2014-10-16 08:12:13 -0500

    Added Activity

--- Stopwatch/Lee/app/src/main/java/leepresswood/com/stopwatch/activities/ActivityMain.java
+++ Stopwatch/Lee/app/src/main/java/leepresswood/com/stopwatch/activities/ActivityMain.java
@@ -1,34 +1,39 @@
 package leepresswood.com.stopwatch.activities;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import leepresswood.com.stopwatch.R;
 
-public class ActivityMain extends ActionBarActivity {
+public class ActivityMain extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
         return super.onOptionsItemSelected(item);
